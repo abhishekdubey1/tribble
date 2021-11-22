6e868b4b5f03ae6c957005e9cc8d4a6d306c4497
Abhishek <ad99526@gmail.com>
2021-11-22 10:31:22 +0530

    Make api call and render list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { axios } from "axios";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
+const url = "https://LonelyBumpyFlatassembler2.ad99526.repl.co";
+
 function App() {
-  return <div className="App"></div>;
+  const [input, setInput] = useState("");
+  const [people, setPeople] = useState([]);
+  const getUsers = useCallback(async () => {
+    const { data } = await axios(`${url}/search/${input}`);
+    if (data.people) {
+      setPeople(data.people);
+    }
+  }, [input]);
+  useEffect(() => {
+    if (input) {
+      getUsers();
+    }
+  }, [getUsers, input]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
