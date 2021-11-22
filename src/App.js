4afb8cf579a20a39338a8539be233d0a97be29bd
Abhishek <ad99526@gmail.com>
2021-11-22 11:01:22 +0530

    Add highlighting component and algorithm - Added a highlighter component which can be used to highlight text - Added a highlighter algorithm which returns a list of ranges to highlight

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import axios from "axios";
 import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
-const url = "https://LonelyBumpyFlatassembler2.ad99526.repl.co";
+const url = "https://lonelybumpyflatassembler2.ad99526.repl.co";
 
 function App() {
   const [input, setInput] = useState("");
   const [people, setPeople] = useState([]);
 
   const getUsers = useCallback(async () => {
-    const { data } = await axios(`${url}/search/${input}`);
-    console.log(data);
-    if (data.people) {
-      setPeople(data.people);
+    try {
+      const { data } = await axios(`${url}/search/${input}`);
+      console.log(data);
+      if (data.people) {
+        setPeople(data.people);
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   }, [input]);
 
@@ -38,3 +42,31 @@ function App() {
 }
 
 export default App;
+
+const getHighlightedText = (text, highlight, className) => {
+  // Split text on highlight term, include term itself into parts, ignore case
+  const parts = text?.split(new RegExp(`(${highlight})`, "gi"));
+  return (
+    <span>
+      {parts?.map((part) =>
+        part.toLowerCase() === highlight.toLowerCase() ? (
+          <span className={className}>{part}</span>
+        ) : (
+          <>{part}</>
+        )
+      )}
+    </span>
+  );
+};
+
+const Highlighter = ({
+  condition,
+  sentence,
+  highlight,
+  className = "highlight",
+}) => {
+  if (condition) {
+    return getHighlightedText(sentence, highlight, className);
+  }
+  return sentence || "";
+};
